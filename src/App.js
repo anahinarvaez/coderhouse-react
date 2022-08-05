a356a99d84e2143c253f70453b30eb790c1ac62b
Anahi Narvaez <anahinarvaezch@gmail.com>
2022-08-04 21:58:47 -0300

    entrega4

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import './App.css';
 import NavBar from './components/navbar/navbar';
 import ItemListContainer from './components/Card/ItemListContainer';
+import ItemCount from './components/Card/ItemCount';
 
 function App() {
   return (
     <div>
       <NavBar></NavBar>
       <ItemListContainer greeting="Elegi tu hamburguesa"/>
+      <ItemCount initial={1} stock={3}/>
     </div>
+
     
   );
+
+  
 }
 
 
